03e6fdd2d2a10bbe80352ff7f2eac0839f57d4b6
devakthk <devakthk@gmail.com>
2018-02-18 14:11:24 +0800

    add django rest api project

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
     include_package_data=True,
     scripts=["manage.py"],
     install_requires=["Django>=1.9,<2.0",
+                      "Click>=6.0",
                       "django-cors-headers>=1.1.0",
                       "djangorestframework>=3.3.1",
                       "MySQL-python>=1.2.5",
