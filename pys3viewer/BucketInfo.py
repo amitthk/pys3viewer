ca3a1ae1bf05e8970de889f085da3df919861024
devakthk <devakthk@gmail.com>
2018-02-27 01:31:15 +0800

    add alternate s3 fetch using client.list_objects_v2(Bucket=bucket)

--- pys3viewer/BucketInfo.py
+++ pys3viewer/BucketInfo.py
@@ -1,7 +1,7 @@
 
 class BucketInfo:
     """Model class for bucket information"""
-    def __init__(self, bucket_name=None, creation_date=None, number_of_files=None, total_size_of_files=None, last_modified=None):
+    def __init__(self, bucket_name=None, creation_date=None, number_of_files=None, total_size_of_files=None, last_modified=None, storage_class=None):
         if bucket_name:
             self.bucket_name = bucket_name
         if creation_date:
@@ -15,6 +15,7 @@ class BucketInfo:
         else:
             self.total_size_of_files = 0
         self.last_modified = last_modified
+        self.storage_class = storage_class
 
     def __str__(self):
         return "bucket_name: {0}, creation_date : {1}, number_of_files : {2}, total_size_of_files : {3}, last_modified : {1}".format(self.bucket_name, self.creation_date, self.number_of_files, self.total_size_of_files, self.last_modified)
@@ -25,5 +26,5 @@ class BucketInfo:
             'creation_date': self.creation_date,
             'number_of_files': self.number_of_files,
             'total_size_of_files': self.total_size_of_files,
-            'last_modified':self.last_modified
+            'last_modified': self.last_modified
         }
--- pys3viewer/BucketManager.py
+++ pys3viewer/BucketManager.py
@@ -45,6 +45,54 @@ class BucketManager:
 
         return bucket_info
 
+    def get_bucket_statistics_v2(self, bucket_name, storageTypeFilter=None):
+        """
+        Fetches the bucket information
+
+        :param
+        bucket_name:
+
+        :return:
+        bucket_info of type BucketInfo
+        """
+        bucket_info = BucketInfo()
+        bucket_info.bucket_name = bucket_name
+        s3 = self.credentials.session.resource('s3')
+        current_bucket = s3.Bucket(bucket_name)
+        bucket_info.creation_date = current_bucket.creation_date
+        #get_object_info = lambda obj: [int(obj['LastModified'].strftime('%s')), obj['Size'], obj['StorageClass']]
+
+        for bucket_object in self.iterate_bucket_objects(bucket=bucket_name):
+            if storageTypeFilter is None:
+                bucket_info.total_size_of_files += int(bucket_object['Size'])
+                _last_modified = bucket_object['LastModified']
+                bucket_info.number_of_files += 1
+                if (bucket_info.last_modified is None) or (_last_modified > bucket_info.last_modified):
+                    bucket_info.last_modified = _last_modified
+            elif storageTypeFilter is bucket_object['StorageClass']:
+                bucket_info.total_size_of_files += int(bucket_object['Size'])
+                _last_modified = bucket_object['LastModified']
+                bucket_info.number_of_files += 1
+                if (bucket_info.last_modified is None) or (_last_modified > bucket_info.last_modified):
+                    bucket_info.last_modified = _last_modified
+
+
+        return bucket_info
+
+    def iterate_bucket_objects(self, bucket):
+        """
+        Generator that iterates over all objects in a given s3 bucket
+        :param bucket: name of s3 bucket
+        :return: dict of metadata for an object
+        """
+        client = self.credentials.session.client('s3')
+        page_iterator = client.list_objects_v2(Bucket=bucket)
+        if 'Contents' not in page_iterator:
+            return []
+        for item in page_iterator['Contents']:
+            yield item
+
+
     def get_files_in_bucket(self, bucket_name):
         """Returns he list of objects within a bucket.
 
@@ -56,4 +104,15 @@ class BucketManager:
         s3 = self.credentials.session.resource('s3')
         this_bucket = s3.Bucket(bucket_name)
         list_of_files = [s3file.key for s3file in this_bucket.objects.all()];
+        return list_of_files
+
+    def get_files_in_bucket_v2(self, bucket_name):
+        """Returns he list of objects within a bucket.
+
+        Args:
+            bucket_name: name of bucket
+        Returns:
+            list of all objects within bucket
+        """
+        list_of_files = [s3file['Key'] for s3file in self.iterate_bucket_objects(bucket=bucket_name)]
         return list_of_files
